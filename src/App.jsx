26350ef44a7669c1a5212f52f1d394c7882b9ed6
Diego Camarillo <bigocam123@gmail.com>
2025-05-13 09:31:35 -0600

    Arreglo de navbar e imagen

--- src/App.jsx
+++ src/App.jsx
@@ -4,19 +4,16 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom"
 import Home from "./pages/Home"
 import NotFound from "./pages/NotFound"
 import "./App.css"
-import SmoothScroll from "./components/SmoothScroll"
 
 function App() {
   return (
     <Router>
-      <SmoothScroll>
         <div className="app-container">
           <Routes>
             <Route path="/" element={<Home />} />
             <Route path="*" element={<NotFound />} />
           </Routes>
         </div>
-      </SmoothScroll>
     </Router>
   )
 }
